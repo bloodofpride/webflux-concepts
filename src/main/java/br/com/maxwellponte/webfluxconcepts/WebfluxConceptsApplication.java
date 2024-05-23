c5182e134371baa70900e9e30427b3d2e05ed405
Antonio Maxwell Ponte Rocha <antonio_maxwell@hotmail.com>
2024-05-23 11:34:52 -0300

    start project

---
+++ src/main/java/br/com/maxwellponte/webfluxconcepts/WebfluxConceptsApplication.java
@@ -0,0 +1,13 @@
+package br.com.maxwellponte.webfluxconcepts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxConceptsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxConceptsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/maxwellponte/webfluxconcepts/WebfluxConceptsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.maxwellponte.webfluxconcepts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxConceptsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
