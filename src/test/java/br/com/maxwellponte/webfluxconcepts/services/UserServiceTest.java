df43416e1b95c04918903127a329dbaf1e25563e
bloodofpride <antonio_maxwell@hotmail.com>
2024-07-26 20:13:08 -0300

    add unit test to findById user service

--- src/test/java/br/com/maxwellponte/webfluxconcepts/services/UserServiceTest.java
+++ src/test/java/br/com/maxwellponte/webfluxconcepts/services/UserServiceTest.java
@@ -16,6 +16,7 @@ import reactor.test.StepVerifier;
 import java.util.Objects;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.when;
 
@@ -32,7 +33,7 @@ class UserServiceTest {
 
 
     @Test
-    void save() {
+    void testSave() {
         UserRequest request = new UserRequest("maxwell", "maxwell@mail.com", "123");
         User entity = User.builder().build();
 
@@ -42,11 +43,26 @@ class UserServiceTest {
         Mono<User> result = service.save(request);
 
         StepVerifier.create(result)
-                .expectNextMatches(Objects::nonNull)
+                .expectNextMatches(user -> user.getClass() == User.class)
                 .expectComplete()
 
                 .verify();
 
         Mockito.verify(repository, times(1)).save(any(User.class));
+    }
+
+    @Test
+    void testFindById(){
+        when(repository.findById(anyString())).thenReturn(Mono.just(User.builder().id("1234").build()));
+
+        Mono<User> result = service.findById("1234");
+
+        StepVerifier.create(result)
+                .expectNextMatches(user -> user.getClass() == User.class
+                && Objects.equals(user.getId(), "1234"))
+                .expectComplete()
+                .verify();
+
+        Mockito.verify(repository, times(1)).findById(anyString());
     }
 }
