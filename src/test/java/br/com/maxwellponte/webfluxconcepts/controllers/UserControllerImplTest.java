eaeb1fcd65c2077e49bb8d8f637868391087ee71
Antonio Maxwell Ponte Rocha <antonio_maxwell@hotmail.com>
2024-08-07 16:48:52 -0300

    add integration test to user save endpoint with succe

---
+++ src/test/java/br/com/maxwellponte/webfluxconcepts/controllers/UserControllerImplTest.java
@@ -0,0 +1,74 @@
+package br.com.maxwellponte.webfluxconcepts.controllers;
+
+import br.com.maxwellponte.webfluxconcepts.entities.User;
+import br.com.maxwellponte.webfluxconcepts.mappers.UserMapper;
+import br.com.maxwellponte.webfluxconcepts.models.requests.UserRequest;
+import br.com.maxwellponte.webfluxconcepts.services.UserService;
+import com.mongodb.reactivestreams.client.MongoClient;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.BodyInserters;
+import reactor.core.publisher.Mono;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureWebTestClient
+class UserControllerImplTest {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @MockBean
+    private UserService userService;
+
+    @MockBean
+    private UserMapper userMapper;
+
+    @MockBean
+    private MongoClient mongoClient;
+
+    @Test
+    @DisplayName("Test endpoint save with success")
+    void testSaveWithSuccess() {
+        UserRequest userRequest = new UserRequest("Maxwell", "maxwell@mail.com", "123");
+
+        when(userService.save(any(UserRequest.class))).thenReturn(Mono.just(User.builder().build()));
+
+        webTestClient.post().uri("/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromValue(userRequest))
+                .exchange()
+                .expectStatus().isCreated();
+
+        Mockito.verify(userService, times(1)).save(any(UserRequest.class));
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
