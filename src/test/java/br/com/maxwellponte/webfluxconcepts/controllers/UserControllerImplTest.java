b1f4599fc3099a8016d4c4cb841371a600544bc2
Antonio Maxwell Ponte Rocha <antonio_maxwell@hotmail.com>
2024-08-09 11:35:00 -0300

    add integration test to user save endpoint with bad request

--- src/test/java/br/com/maxwellponte/webfluxconcepts/controllers/UserControllerImplTest.java
+++ src/test/java/br/com/maxwellponte/webfluxconcepts/controllers/UserControllerImplTest.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
@@ -54,6 +55,27 @@ class UserControllerImplTest {
                 .expectStatus().isCreated();
 
         Mockito.verify(userService, times(1)).save(any(UserRequest.class));
+    }
+
+    @Test
+    @DisplayName("Test endpoint save with bad request")
+    void testSaveWithBadRequest() {
+        UserRequest userRequest = new UserRequest(" Maxwell", "maxwell@mail.com", "123");
+
+        webTestClient.post().uri("/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromValue(userRequest))
+                .exchange()
+                .expectStatus().isBadRequest()
+                .expectBody()
+                .jsonPath("$.path").isEqualTo("/users")
+                .jsonPath("$.status").isEqualTo(HttpStatus.BAD_REQUEST.value())
+                .jsonPath("$.error").isEqualTo("Validation Errors")
+                .jsonPath("$.message").isEqualTo("Error on validation attributes")
+                .jsonPath("$.errors[0].fieldName").isEqualTo("name")
+                .jsonPath("$.errors[0].message").isEqualTo("field cannot have blank spaces at the beginning or at end");
+
+        Mockito.verify(userService, times(0)).save(any(UserRequest.class));
     }
 
     @Test
